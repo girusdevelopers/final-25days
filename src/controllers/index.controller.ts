c7b464eeaed3c3f8f5b767b177e5365a890a3ecb
Mahendrakoppula <mahendrakoppula09@gamil.com>
2023-12-04 10:45:44 +0530

    index file changed

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -7,7 +7,7 @@ export const home = (req: Request, res: Response, next: NextFunction) => {
    // Respond with a 200 status and a JSON message 
     return res.status(200).json({
       success: true,
-      message: 'Hello World!😂😍🎉🐕😶‍🌫️😡🤬🤡👺🤯🐱‍🐉🤢🤔😆✌',
+      message: 'Hello ameer',
     });
   } catch (error) {
    // If an error occurs, pass it to the error-handling middleware 
