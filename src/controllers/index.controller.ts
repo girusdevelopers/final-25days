01fa798b870000ad78123e46d667ad9e0293d902
Girus <141259525+girusdevelopers@users.noreply.github.com>
2023-12-05 19:46:48 +0530

    Update index.controller.ts

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -7,7 +7,7 @@ export const home = (req: Request, res: Response, next: NextFunction) => {
    // Respond with a 200 status and a JSON message 
     return res.status(200).json({
       success: true,
-      message: 'Hello ameer',
+      message: 'Hello cicd',
     });
   } catch (error) {
    // If an error occurs, pass it to the error-handling middleware 
