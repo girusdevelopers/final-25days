e94837cb27ed2590db329708dd6a08b7db704ca4
raju reddy <raju1997reddy@gmail.com>
2023-12-07 18:29:41 +0530

    subfoldervideo changes

--- src/routes/SubFolderVideo/MainFolder.route.ts
+++ src/routes/SubFolderVideo/MainFolder.route.ts
@@ -1,18 +1,32 @@
+// import express from 'express';
+// import * as videoMainFolder from '@controllers/SubFolderVideo/MainFolder.controller'
+// import { upload } from '@/utils/s3service';
+
+// const router = express.Router();
+
+// // Define routes for article operations
+// router.post('/create',upload.single('MainFolderBanner'), videoMainFolder.createMainFolder); // Create a new article
+// router.get('/getall', videoMainFolder.allmainvieofolders); // Get all articles
+// // // router.get('/:id', Article.getArticleById); // Get a specific article by ID
+// // router.get('/:MainFolderName',MainFolder.getMainFolderByName);//Get a specific article by word
+// // router.put('/Update/a:id', Album.UpdateAlbumTitle); // Update an article by ID
+// // router.delete('/delete/:id', Album.deleteAlbumById); // Delete an article by ID
+
+// // router.delete('/delete/:id', Article.deleteArticleById);//deleteById
+
+
+// export default router;
+
 import express from 'express';
-import * as videoMainFolder from '@controllers/SubFolderVideo/MainFolder.controller'
+import * as MainFolder from '@controllers/SubFolderVideo/MainFolder.controller'
 import { upload } from '@/utils/s3service';
 
 const router = express.Router();
-
-// Define routes for article operations
-router.post('/create',upload.single('MainFolderBanner'), videoMainFolder.createMainFolder); // Create a new article
-router.get('/getall', videoMainFolder.allmainvieofolders); // Get all articles
-// // router.get('/:id', Article.getArticleById); // Get a specific article by ID
-// router.get('/:MainFolderName',MainFolder.getMainFolderByName);//Get a specific article by word
-// router.put('/Update/a:id', Album.UpdateAlbumTitle); // Update an article by ID
-// router.delete('/delete/:id', Album.deleteAlbumById); // Delete an article by ID
-
-// router.delete('/delete/:id', Article.deleteArticleById);//deleteById
-
+ 
+router.post('/create',upload.single('MainFolderBanner'), MainFolder.createMainFolder);  
+router.get('/getall', MainFolder.getAllMainFolders); 
+router.get('/mainfolders/:MainmostFolderName',MainFolder.getMainFolderByName);
+router.delete('/deletebyname/:MainmostFolderName', MainFolder.deleteMainFolder)
+ 
 
 export default router;
--- src/routes/SubFolderVideo/Video.route.ts
+++ src/routes/SubFolderVideo/Video.route.ts
@@ -1,15 +1,33 @@
+// import express, { RequestHandler } from "express";
+// import * as uploadVideo from "@controllers/SubFolderVideo/Video.controller";
+// import { upload } from "@/utils/s3service";
+ 
+
+// const router = express.Router();
+
+// // Define routes for article operations
+// router.post("/upload",uploadVideo.uploadVideo); //upload
+// // router.get('/getall', Album.getallAlbums); // Get all articles
+// // // router.get('/:id', Article.getArticleById); // Get a specific article by ID
+// router.get("/main/:MainFolderName", uploadVideo.getMainFolderByName); //Get a specific article by word
+// router.get("/sub/:SubFolderName", uploadVideo.getsubFolderByName); //Get a specific article by word
+  
+// export default router;
+
 import express, { RequestHandler } from "express";
 import * as uploadVideo from "@controllers/SubFolderVideo/Video.controller";
 import { upload } from "@/utils/s3service";
  
 
 const router = express.Router();
+ 
+router.post("/upload",uploadVideo.uploadVideo);  
+ router.get("/main/:MainFolderName", uploadVideo.getMainFolderByName);  
+router.get("/sub/:SubFolderName", uploadVideo.getsubFolderByName);  
+router.get("/getAllVideos",uploadVideo.getAllVideos);
+router.get("/getVideoByTitle/:Videotitle", uploadVideo.getVideoByTitle);
+router.delete("/deleteVideoByTitle/:Videotitle", uploadVideo.deleteVideoByTitle);
 
-// Define routes for article operations
-router.post("/upload",uploadVideo.uploadVideo); //upload
-// router.get('/getall', Album.getallAlbums); // Get all articles
-// // router.get('/:id', Article.getArticleById); // Get a specific article by ID
-router.get("/main/:MainFolderName", uploadVideo.getMainFolderByName); //Get a specific article by word
-router.get("/sub/:SubFolderName", uploadVideo.getsubFolderByName); //Get a specific article by word
-  
+
 export default router;
+
